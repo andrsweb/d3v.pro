dfba8973168377a5f9f7616aaa3ee213db5dd943
andrsweb <d3vv.proo@gmail.com>
2024-04-02 04:55:19 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="container">
+          <p>d3v.pro</p>
+          <h2>For heading</h2>
+          <div>Simple text</div>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default App
